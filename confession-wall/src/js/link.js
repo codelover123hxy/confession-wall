06b45ca77dfe2ae6504d05ac03cd2916f407dabf
codelover123hxy <97789400+codelover123hxy@users.noreply.github.com>
2022-09-18 23:42:33 +0800

    Add files via upload

---
+++ confession-wall/src/js/link.js
@@ -0,0 +1,12 @@
+function returnBack(){
+    window.location.href="../index.html"
+}
+
+function gotoLogin(){
+    window.location.href="/login/index.html"
+}
+
+function gotoRegister(){
+    console.log("1");
+    window.location.href="/register/index.html"
+}
